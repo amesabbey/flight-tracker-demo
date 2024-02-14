e09c2dcf927b9a084a422ad047f6c48b092c7720
amesabbey <amesabbey@gmail.com>
2024-02-14 15:13:32 -0700

    Saved flights grid

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FlightFormComponent } from "../flight-form/flight-form.component";
+import { SavedFlightsComponent } from "../saved-flights/saved-flights.component";
 
 @Component({
     selector: 'app-dashboard',
     standalone: true,
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.scss',
-    imports: [FlightFormComponent]
+    imports: [FlightFormComponent, SavedFlightsComponent]
 })
 export class DashboardComponent {
 
